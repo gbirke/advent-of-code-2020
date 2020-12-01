c6d6c4af5fe262fb946a9998da3b926fb86ddd73
Gabriel Birke <gabriel.birke@gmail.com>
2020-12-01 22:48:04 +0100

    Change Rust setup to cargo-aoc
    Use daily solution as decorated library.
    
    Add README
    
    Thanks to https://github.com/gobanos/cargo-aoc

---
+++ Rust/README.md
@@ -0,0 +1,17 @@
+# Rust solutions 
+
+## Run all solutions:
+
+	cargo aoc
+	
+## Run one day
+
+	cargo aoc -d {day} -p {part}
+
+## Fetch inputs
+
+	cargo aoc input
+	
+(You need to have set up your credentials)
+
+See https://github.com/gobanos/cargo-aoc for more information
--- Rust/src/day1.rs
+++ Rust/src/day1.rs
@@ -1,13 +1,13 @@
 use std::collections::HashMap;
-use std::fs;
 use std::iter;
 
-fn read_input_numbers() -> Vec<i64> {
-    let content = fs::read_to_string("input.txt").expect("input.txt not found");
-    content.lines().map(|l| l.parse::<i64>().unwrap()).collect()
+#[aoc_generator(day1)]
+pub fn read_input_numbers(input: &str) -> Vec<i64> {
+    input.lines().map(|l| l.parse::<i64>().unwrap()).collect()
 }
 
-fn part1(numbers: &[i64]) {
+#[aoc(day1, part1)]
+fn part1(numbers: &[i64]) -> i64 {
     // a lookup table for [2020-n] => n
     let numbers_and_rests: HashMap<_, _> = numbers
         .iter()
@@ -26,9 +26,13 @@ fn part1(numbers: &[i64]) {
         solution.1,
         solution.0 * *solution.1
     );
+
+
+    solution.0 * *solution.1
 }
 
-fn part2(numbers: &[i64]) {
+#[aoc(day1, part2)]
+fn part2(numbers: &[i64]) -> i64 {
     let lowest_number = numbers.iter().min().unwrap();
     // Create tuples of the cartesian product of numbers, leaving out existing
     // combinations by using an index.
@@ -52,11 +56,7 @@ fn part2(numbers: &[i64]) {
         solution.2,
         solution.0 * solution.1 * solution.2
     );
+
+    solution.0 * solution.1 * solution.2
 }
 
-fn main() {
-    // let test_numbers = vec![1721, 979, 366, 299, 675, 145];
-    let input_numbers = &read_input_numbers();
-    part1(input_numbers);
-    part2(input_numbers);
-}
